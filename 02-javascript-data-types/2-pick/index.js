34792bb3064db06a870d1706a88e5be9c253121b
PavelSidyuk <76783639+PavelSidyuk@users.noreply.github.com>
2024-01-10 11:31:20 +0400

    lesson 02-javascript-data-types

--- 02-javascript-data-types/2-pick/index.js
+++ 02-javascript-data-types/2-pick/index.js
@@ -5,5 +5,11 @@
  * @returns {object} - returns the new object
  */
 export const pick = (obj, ...fields) => {
-
+  for (const objKey in obj) {
+    if (!(fields.includes(objKey))){
+      delete obj[objKey];
+    }
+  }
+  return obj;
 };
+
