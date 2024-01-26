16136df00a5b94f403cf3571dd51f56d9978b993
PavelSidyuk <76783639+PavelSidyuk@users.noreply.github.com>
2024-01-26 09:57:55 +0400

    lesson 03

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -4,6 +4,34 @@
  * @param {number} size - the allowed size of consecutive identical symbols
  * @returns {string} - the new string without extra symbols according passed size
  */
-export function trimSymbols(string, size) {
 
+// не помню какой знак для строгой проверки, проверьте по полной программе, хочу что бы код был по возможности короче
+export function trimSymbols(string, size) {
+  let counter = 0;
+  let currentLetter = '';
+  let newStr = '';
+  if (size === 0) {
+    return newStr;
+  }
+  if (size === undefined) {
+    return newStr = string;
+  }
+  for (const strElement of string) {
+    if (currentLetter === '') {
+      currentLetter = strElement;
+      newStr = currentLetter;
+      counter++;
+
+    }
+    if (currentLetter === strElement && counter < size) {
+      newStr = newStr + strElement;
+      counter++;
+    }
+    if (currentLetter !== strElement) {
+      currentLetter = strElement;
+      newStr = newStr + currentLetter;
+      counter = 1;
+    }
+  }
+  return newStr;
 }
