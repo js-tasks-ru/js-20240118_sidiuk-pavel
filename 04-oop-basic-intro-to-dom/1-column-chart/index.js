1c97a799047610ef0c84ba62ad7d5f3e378a7054
PavelSidyuk <76783639+PavelSidyuk@users.noreply.github.com>
2024-01-30 12:05:28 +0400

    lesson 03

--- 04-oop-basic-intro-to-dom/1-column-chart/index.js
+++ 04-oop-basic-intro-to-dom/1-column-chart/index.js
@@ -1,3 +1,86 @@
 export default class ColumnChart {
+  element;
+  chartHeight = 50;
 
+  constructor({
+    data = [],
+    label = '',
+    value = 0,
+    link = '',
+    formatHeading = value => value,
+  } = {}) {
+    this.label = label;
+    this.data = data;
+    this.value = value;
+    this.link = link;
+    this.formatHeading = formatHeading;
+    this.element = this.creatElement(this.createTemplate());
+  }
+
+  creatElement(template) {
+    const element = document.createElement('div');
+    element.innerHTML = template;
+    return element.firstElementChild;
+  }
+
+  getColumnProps() {
+    const maxValue = Math.max(...this.data);
+    const scale = 50 / maxValue;
+
+    return this.data.map(item => {
+      return {
+        percent: (item / maxValue * 100).toFixed(0) + '%',
+        value: String(Math.floor(item * scale))
+      };
+    });
+  }
+
+  createLinkTemplate() {
+    if (this.link) {
+      return `<a href="${this.link}" class="column-chart__link">View all</a>`;
+    }
+    return '';
+
+  }
+
+  createChartBodyTemplate() {
+    return this.getColumnProps().map(({percent, value}) => (
+      `<div style="--value: ${value}" data-tooltip="${percent}"></div>`
+    )).join('');
+
+  }
+
+  createChartClasses() {
+    return this.data.length ? 'column-chart' : 'column-chart column-chart_loading';
+
+  }
+
+  createTemplate() {
+    return (`
+    <div class="${this.createChartClasses()}" style="--chart-height: 50">
+      <div class="column-chart__title">
+        ${this.label}
+        ${this.createLinkTemplate()}
+      </div>
+      <div class="column-chart__container">
+        <div data-element="header" class="column-chart__header">${this.formatHeading(this.value)}</div>
+        <div data-element="body" class="column-chart__chart">
+          ${this.createChartBodyTemplate()}
+        </div>
+      </div>
+    </div>`);
+  }
+
+  update(newDate) {
+    this.data = newDate;
+    this.element = document.querySelector('[data-element="body"]').innerHTML = this.createChartBodyTemplate();
+  }
+
+  remove() {
+    this.element.remove();
+  }
+
+  destroy() {
+    this.remove();
+  }
 }
