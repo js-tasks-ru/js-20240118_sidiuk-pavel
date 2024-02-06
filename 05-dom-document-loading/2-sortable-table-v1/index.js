89b6ff7b92ff280021f29f5016469965829a69f9
PavelSidyuk <76783639+PavelSidyuk@users.noreply.github.com>
2024-02-06 13:57:52 +0400

    lesson 03

--- 05-dom-document-loading/2-sortable-table-v1/index.js
+++ 05-dom-document-loading/2-sortable-table-v1/index.js
@@ -5,8 +5,11 @@ export default class SortableTable {
   constructor(headerConfig = [], data = []) {
     this.headerConfig = headerConfig;
     this.data = data;
-    this.subElements = ''; // не могу понять для чего это поле оно есть в тестах
     this.element = this.creatElement(this.createTemplate());
+    this.subElements = {
+      body: this.element.querySelector('[data-element="body"]')
+    };
+
   }
 
   creatElement(template) {
@@ -15,47 +18,42 @@ export default class SortableTable {
     return element.firstElementChild;
   }
 
+
   getHeaderProps(list) {
-    return list.map(item => this.renderHeader(item.id, item.title, item.sortable, item.sortType, item.template)).join('');
+    return list.map(item => this.renderHeader(item)).join('');
   }
 
   getItemProps(list) {
-    return list.map(item => this.renderItem(item.images, item.title, item.quantity, item.price, item.sales)).join('');
+    return list.map(item => this.renderItem(item)).join('');
   }
 
   renderImg(list) {
-    // if (list[0].url) {
-    //   return `
-    //     <div class="sortable-table__cell">
-    //         <img class="sortable-table-image" alt="Image" src="${list[0].url}">
-    //       </div>
-    //     `;
-    // } else {
-    //Не понимаю почему код выше не проходит тест поле list[0].url существует
-    return `
+    return (`
         <div class="sortable-table__cell">
-            <img class="sortable-table-image" alt="Image" src="'https://via.placeholder.com/32'">
+            <img class="sortable-table-image" alt="Image" src="https://via.placeholder.com/32">
           </div>
-        `;
-    // }
+        `);
+
   }
 
-  renderItem(images, title, quantity, price, sales) {
+  renderItem(item) {
+
+
     return `
        <a href="/products/3d-ochki-epson-elpgs03" class="sortable-table__row">
-        ${this.renderImg(images)}
-        <div class="sortable-table__cell">${title}</div>
-        <div class="sortable-table__cell">${quantity}</div>
-        <div class="sortable-table__cell">${price}</div>
-        <div class="sortable-table__cell">${sales}</div>
-
+        ${this.renderImg(item.images)}
+        <div class="sortable-table__cell">${item.title}</div>
+        <div class="sortable-table__cell">${item.quantity}</div>
+        <div class="sortable-table__cell">${item.price}</div>
+        <div class="sortable-table__cell">${item.sales}</div>
       </a>`;
   }
 
-  renderHeader(id, title, sortable, sortType,) {
+  renderHeader(item) {
+
     return `
-        <div class="sortable-table__cell" data-id="${id}" data-sortable="${sortable}" data-order="${sortType}">
-        <span>${title}</span>
+        <div class="sortable-table__cell" data-id="${item.id}" data-sortable="${item.sortable}" data-order="${item.sortType}">
+        <span>${item.title}</span>
         <span data-element="arrow" class="sortable-table__sort-arrow">
           <span class="sort-arrow"></span>
         </span>
@@ -63,6 +61,7 @@ export default class SortableTable {
   }
 
   createTemplate() {
+    console.log(this.headerConfig)
     return (`
 <div data-element="productsContainer" class="products-list__container">
   <div class="sortable-table">
@@ -86,15 +85,12 @@ export default class SortableTable {
   }
 
 
-  sort(field, param = 'asc') {
-
+  sort(field, param) {
     if (param === 'asc') {
       this.data = this.data.sort(function (a, b) {
-        console.log(typeof a[field]);
         if (typeof a[field] === 'string') {
           return a[field].localeCompare(b[field], ['ru', 'en-US'], {caseFirst: 'upper'});
         } else {
-          console.log(typeof a[field]);
           if (a[field] > b[field]) {
             return 1;
           }
@@ -108,11 +104,9 @@ export default class SortableTable {
     }
     if (param === 'desc') {
       this.data = this.data.sort(function (a, b) {
-        console.log(typeof a[field]);
         if (typeof a[field] === 'string') {
           return b[field].localeCompare(a[field], ['ru', 'en-US'], {caseFirst: 'upper'});
         } else {
-          console.log(typeof a[field]);
           if (b[field] > a[field]) {
             return 1;
           }
@@ -121,11 +115,10 @@ export default class SortableTable {
           }
           return 0;
         }
-
       });
     }
 
-    this.element.querySelector('[data-element="body"]').innerHTML = this.getItemProps(this.data);
+    this.subElements.body.innerHTML = this.getItemProps(this.data);
 
   }
 
