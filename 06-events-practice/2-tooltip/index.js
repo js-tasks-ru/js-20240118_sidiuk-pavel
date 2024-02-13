bf2862e3d37feba1279e45ec0752433540e34a32
PavelSidyuk <76783639+PavelSidyuk@users.noreply.github.com>
2024-02-13 12:38:36 +0400

    lesson 06

--- 06-events-practice/2-tooltip/index.js
+++ 06-events-practice/2-tooltip/index.js
@@ -1,7 +1,65 @@
 class Tooltip {
-  initialize () {
+  element;
+  static lastTooltipElement;
+
+  initialize() {
+
+    this.body = document.querySelector('body');
+    this.element = this.createElement(this.createTemplate());
+    this.createEventListeners();
+    if (Tooltip.lastTooltipElement) {
+      Tooltip.lastTooltipElement.destroy();
+    }
+    Tooltip.lastTooltipElement = this;
 
   }
+
+  createEventListeners() {
+    this.body.addEventListener('pointerover', this.pointeroverEvent);
+  }
+  destroyEventListeners() {
+    this.body.removeEventListener('pointerover', this.pointeroverEvent);
+  }
+  pointeroverEvent = () => {
+    this.body.addEventListener('pointerover', (event) =>{
+      if (event.target.dataset.tooltip) {
+        this.element.innerText = event.target.dataset.tooltip;
+        this.element.style.top = `${event.clientY}px`;
+        this.element.style.left = `${event.clientX}px`;
+        this.render();
+      }
+    });
+
+  }
+
+  createElement(template) {
+    const element = document.createElement('div');
+    element.innerHTML = template;
+    return element.firstElementChild;
+  }
+
+  createTemplate() {
+    return `<div class="tooltip"></div>`;
+  }
+
+  render() {
+
+
+
+    this.body.appendChild(this.element);
+
+  }
+
+  remove() {
+    this.element.remove();
+  }
+
+  destroy() {
+    this.remove();
+    this.destroyEventListeners();
+
+  }
+
 }
 
 export default Tooltip;
